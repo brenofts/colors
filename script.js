2c73095c6ed0e6e2a8c5fbc96eff1a29c03d9047
brenofts <brenofts@gmail.com>
2021-06-04 00:49:37 -0300

    input name animation

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var menu = document.querySelector('.menu')
 var modal = document.getElementById('modal')
 var input = document.querySelectorAll('input')
 var body = document.querySelector('body')
+var inputName = document.getElementById('inputName')
 
 blueValue.addEventListener('click', () => {
   modal.classList.replace('hidden', 'modal')
@@ -19,7 +20,7 @@ blueValue.addEventListener('click', () => {
 document.getElementById('red').addEventListener('input', (e) => {
   var redSet = 'rgb(' + e.target.value + ', 0, 0)'
   redValue.style.backgroundColor = redSet
-  input[0].style.backgroundColor = redSet
+  input[1].style.backgroundColor = redSet
   r = e.target.value
   rgb = "rgb(" + r + ', ' + g + ", " + b + ")"
   body.style.backgroundColor = rgb
@@ -28,17 +29,21 @@ document.getElementById('red').addEventListener('input', (e) => {
 document.getElementById('green').addEventListener('input', (e) => {
   var greenSet = 'rgb(0, ' + e.target.value + ', 0)'
   greenValue.style.backgroundColor = greenSet
-  input[1].style.backgroundColor = greenSet
+  input[2].style.backgroundColor = greenSet
   g = e.target.value
   rgb = "rgb(" + r + ', ' + g + ", " + b + ")"
   body.style.backgroundColor = rgb
+  inputName.style.animation = 'hideInput 0.5s'
+  inputName.style.marginTop = '-10vh'
 })
 
 document.getElementById('blue').addEventListener('input', (e) => {
   var blueSet = "rgb(0, 0, " + e.target.value + ")"
   blueValue.style.backgroundColor = blueSet
-  input[2].style.backgroundColor = blueSet
+  input[3].style.backgroundColor = blueSet
   b = e.target.value
   rgb = "rgb(" + r + ', ' + g + ", " + b + ")"
   body.style.backgroundColor = rgb
+  inputName.style.animation = 'showInput 1s'
+  inputName.style.marginTop = '2vh'
 })
